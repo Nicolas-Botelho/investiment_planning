46bc0f874fd4fa8d71f4449afcee1a76f6c892f7
Nicolas-Botelho <nicolasbotelho2005@gmail.com>
2025-09-26 16:45:41 -0300

    fix: solving warnings

--- src/components/proj_patr_card.tsx
+++ src/components/proj_patr_card.tsx
@@ -5,6 +5,8 @@ import { GlobalContext } from "./context";
 export function PPCard() {
   const context = useContext(GlobalContext)
 
+  console.log(context.total)
+
   return (
     <div className="card">
       <h2>Projeção do Patrimonio para o Proximo Ano</h2>
